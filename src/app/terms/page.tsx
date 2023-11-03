a3855116ab53e3e34f218cad0aa2c4549ba5a7cb
Rony Philip <ronyphilip751@gmail.com>
2023-11-03 08:47:38 +0000

    add terms of service

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,14 @@
+function Page() {
+  return (
+    <main className="flex items-center justify-center h-screen fade-in">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl font-semibold">Terms of Service</h1>
+        <ul>
+          <li>Soon™️</li>
+        </ul>
+      </div>
+    </main>
+  )
+}
+
+export default Page;
